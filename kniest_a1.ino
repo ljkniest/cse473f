44c58cb0331ec46287d79b5946f62eed81ace50c
ljkniest <73071992+ljkniest@users.noreply.github.com>
2024-04-11 01:53:50 -0700

    Upload A1 .ino

---
+++ kniest_a1.ino
@@ -0,0 +1,207 @@
+// This program is a nightlight that changes color based on the resistor attached and
+// uses arrays of brake data pulled from a self-authored python script.
+// Uses fastLED library, needs to be installed via arduino library manager.
+// Authored April 2024 - Lianne Kniest, CSE 473F Sp 2024
+
+// Inspired by https://github.com/marmilicious/FastLED_examples/blob/master/AnalogBasicSetup.ino
+// for the usage of FastLED to light up the RGB led and color shifting mode.
+// Also inspired by https://www.instructables.com/Arduino-Interrupts/ for arduino interrupt functions.
+// Also inspired by the course textbook, https://makeabilitylab.github.io/physcomp/
+
+// This assumes that the LED used is common cathode. 
+
+#include <FastLED.h>
+
+#define BUTTON_PIN 2
+#define RED_PIN   3
+#define GREEN_PIN 5
+#define BLUE_PIN  6
+#define BRAKE_PIN 10
+#define PHOTO_PIN A0
+#define TRACK_PIN A1
+
+// macros for brake light behavior
+#define MONZA 1
+#define MONACO 2
+#define SPA 3
+
+// macros for modes
+#define RAINBOW 0
+#define NO_LIGHT 1
+#define B_LIGHT 2
+
+// debounce value ms
+#define DEBOUNCE_DELAY 500
+volatile unsigned long last_debounce = 0;
+
+// tuning potentiometer values
+uint16_t DAYLIGHT = 300;
+uint16_t DARK = 50;
+int LIGHT_RANGE = DAYLIGHT - DARK;
+
+// LED values
+uint8_t MAX_BRIGHTNESS = 255; // adjust up to 255 to change max brightness of LED
+CRGB leds[1]; // we are using one analog RGB LED, not a strand
+uint8_t hue;  // will be used to save hue status of LED
+float brightness;  // 0-1 to define brightness for crossfading
+uint8_t hue_delta = 5;
+
+// state machine values
+uint8_t TRACK_MODE = MONZA;
+uint8_t BUTTON_MODE = RAINBOW;
+
+
+// brake light data
+static uint16_t BRAKE_DATA_LEN = 852;
+uint8_t monza_brake_data[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+uint8_t monaco_brake_data[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+uint8_t spa_brake_data[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+uint16_t brake_light_ctr = 0;
+
+void setup() {
+  // Set the RGB pins to output
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
+  pinMode(BRAKE_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), mode_switch, FALLING);
+  startupFlash();
+  Serial.begin(9600);
+  delay(100);
+}
+
+void loop() {
+  EVERY_N_MILLISECONDS(1000) {
+    poll_track();
+  }
+
+  EVERY_N_MILLISECONDS(100) {
+    // brake light
+    uint8_t* brake_data = monza_brake_data;
+    if (BUTTON_MODE == RAINBOW || BUTTON_MODE == NO_LIGHT) {
+      analogWrite(BRAKE_PIN, 0);
+      return;
+    }
+    if (TRACK_MODE == MONZA) {
+      brake_data = monza_brake_data;
+    }
+    if (TRACK_MODE == MONACO) {
+      brake_data = monaco_brake_data;
+    }
+    else {
+      brake_data = spa_brake_data;
+    }
+
+    if (brake_light_ctr > BRAKE_DATA_LEN) {
+      brake_light_ctr = 0;
+    }
+    if (brake_data[brake_light_ctr] == 1) {
+      // Serial.println("high");
+      analogWrite(BRAKE_PIN, MAX_BRIGHTNESS);
+    } else {
+      // Serial.print("low");
+      analogWrite(BRAKE_PIN, 0);
+    }
+    brake_light_ctr += 1;
+  }
+
+
+  EVERY_N_MILLISECONDS(10) {
+    // choose brightness and change RGB color
+    brightness = getBrightness();
+    if (BUTTON_MODE == RAINBOW) {
+      uint8_t hue = beat8(hue_delta, 255);
+      fill_rainbow(leds, 1, hue, hue_delta);     
+    } else {
+      // track-based
+      if (TRACK_MODE == MONZA) {
+        fill_solid(leds,1,CRGB::Green);
+      } else if (TRACK_MODE == MONACO) {
+        fill_solid(leds,1,CRGB::Red);
+      } else {
+        // spa
+        fill_solid(leds,1,CRGB::Purple);
+      }
+    }
+    // Serial.println(brightness);
+    outputRGB(leds[0], brightness);  // update the LED display
+  } 
+
+  delay(10);
+}
+
+
+void poll_track() {
+  int val = analogRead(TRACK_PIN);
+  float voltage = val * (5.0 / 1023.0);
+  if (voltage > 2.5) {
+    TRACK_MODE = MONACO;
+  } else if (voltage <= 2.5 && voltage > 2.0) {
+    TRACK_MODE = SPA;
+  } else {
+    TRACK_MODE = MONZA;
+  }
+  // Serial.println(voltage);
+}
+
+float getBrightness() {
+  int val = analogRead(PHOTO_PIN);  // read the input pin
+  float toReturn = val * 1.0 / (LIGHT_RANGE);  // will be offset by +DARK so it never goes truly to 0
+  // Serial.println(val);          // debug value
+  // Serial.println(toReturn);  // prints brightness
+  return min(1.0, toReturn);
+}
+
+// Output the given color settings
+void outputRGB(const CRGB& rgb, float bright_modifier) {
+  //Note: scale8 is used to adjust values based on BRIGHTNESS
+  uint8_t bright = (int) (bright_modifier * MAX_BRIGHTNESS);
+  // Serial.println(bright);
+  // Serial.println(bright_modifier);
+  analogWrite(RED_PIN,   scale8(rgb.r, bright) );  //for common cathode
+  analogWrite(GREEN_PIN, scale8(rgb.g, bright) );
+  analogWrite(BLUE_PIN,  scale8(rgb.b, bright) );
+}
+
+// Flash every LED color in sequence to prove that it works
+void startupFlash() {
+  fill_solid(leds,1,CRGB::Red);
+  outputRGB(leds[0], 1); delay(1500);
+  fill_solid(leds,1,CRGB::Green);
+  outputRGB(leds[0], 1); delay(1500);
+  fill_solid(leds,1,CRGB::Blue);  
+  outputRGB(leds[0], 1); delay(1500);
+}
+
+void mode_switch() {
+  if ((millis() - last_debounce) > DEBOUNCE_DELAY) {
+    // Serial.println("debounced button press");
+    last_debounce = millis();
+    BUTTON_MODE += 1;
+    if (BUTTON_MODE > B_LIGHT) {
+      BUTTON_MODE = RAINBOW;
+    }
+    brake_light_ctr = 0;  // reset course
+    if (BUTTON_MODE == RAINBOW) {
+      Serial.println("Rainbow");
+    } else if (BUTTON_MODE == B_LIGHT) {
+      Serial.println("Track - w/ brake light");
+    } else {
+      Serial.println("Track - no brake light");
+    }
+  }
+}
