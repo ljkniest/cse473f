08ce71cb88f7fe7f0f4dc79d86a0064fd9711739
ljkniest <ljkniest@uw.edu>
2024-05-21 12:11:35 -0700

    I am sad

--- fp/sketch.js
+++ fp/sketch.js
@@ -11,12 +11,25 @@
 // This code was also iterated on, inspired by, and partially sourced using
 // ChatGPT 3.5 https://chatgpt.com
 
-let pHtmlMsg;
-let serialOptions = { baudRate: 115200  };
-let serial;
+// video configurations
+// const width = 1280;
+// const height = 720;
+// const width = 640;
+// const height = 480;
+
+let video;
+let constraints = {
+  video: {
+    mandatory: {
+      minWidth: 640,
+      minHeight: 480
+    },
+    optional: [{ maxFrameRate: 30 }]
+  },
+  audio: false
+};
 
 // pose net variables
-let video
 let poseNet;
 let currentPoses;
 let poseNetModelReady;
@@ -29,7 +42,7 @@ const poseNetOptions = {
   maxPoseDetections: 5,
   scoreThreshold: 0.5,
   nmsRadius: 20,
-  detectionType: 'single',
+  detectionType: 'multiple',
   inputResolution: 513,
   multiplier: 0.75,
   quantBytes: 2,
@@ -37,22 +50,8 @@ const poseNetOptions = {
 
 function setup() {
   poseNetModelReady = false;
-
-  // Setup Web Serial using serial.js
-  // serial = new Serial();
-  // serial.on(SerialEvents.CONNECTION_OPENED, onSerialConnectionOpened);
-  // serial.on(SerialEvents.CONNECTION_CLOSED, onSerialConnectionClosed);
-  // serial.on(SerialEvents.DATA_RECEIVED, onSerialDataReceived);
-  // serial.on(SerialEvents.ERROR_OCCURRED, onSerialErrorOccurred);
-  
-  // If we have previously approved ports, attempt to connect with them
-  // serial.autoConnectAndOpenPreviouslyApprovedPort(serialOptions);
-  
-  // Add in a lil <p> element to provide messages. This is optional
-  // pHtmlMsg = createP("Click anywhere on this page to open the serial connection dialog");
-
   // PoseNet and camera init
-  video = createCapture(VIDEO);
+  video = createCapture(constraints);
   video.hide();
   poseNet = ml5.poseNet(video, poseNetOptions, onPoseNetModelReady);
   poseNet.on('pose', onPoseDetected);
@@ -67,22 +66,18 @@ function draw() {
     fill(255);
     noStroke();
     text("Waiting for PoseNet model to load...", width/2, height/2);
-  }
-   
-  image(video, 0, 0); // draw the video to the screen at 0,0
-  // if(currentPoses){
-  //   for(let human of currentPoses){
-  //     fill("red"); // red nose
-  //     noStroke();
-  //     circle(human.pose.nose.x, human.pose.nose.y, 40);
-  //   }
-  // }
-    // Iterate through all poses and print them out
-  if(currentPoses){
-    for (let i = 0; i < currentPoses.length; i++) {
-      drawPose(currentPoses[i], i);
+  } else {
+    // Render normal poseNet items
+    image(video, 0, 0, width, height); // draw the video to the screen at 0,0
+    // Iterate through all people and process their bounding boxes
+    if(currentPoses){
+      drawKeypoints(currentPoses);
+      for (let i = 0; i < currentPoses.length; i++) {
+        // boundPerson(currentPoses[i], i);
+      }
     }
   }
+   
 }
 
 function onPoseNetModelReady() {
@@ -97,32 +92,66 @@ function onPoseNetModelReady() {
 function onPoseDetected(poses) {
   console.log("On new poses detected!");
   currentPoses = poses;
-  // if(currentPoses){
-  //   let strHuman = " human";
-  //   if(currentPoses.length > 1){
-  //     strHuman += 's';
-  //   }
-  //   text("We found " + currentPoses.length + strHuman);
-  // }
-  // Iterate through all poses and print them out
-  if(currentPoses){
-    for (let i = 0; i < currentPoses.length; i++) {
-      drawPose(currentPoses[i], i);
-    }
-  }
+}
+
+function boundPerson(pose, poseIndex) {
+  const kpFillColor = color(255, 255, 255, 200);
+  const textColor = color(255, 255, 255, 230);
+  const kpOutlineColor = color(0, 0, 0, 150);
+
+  // const keypoints = pose.pose.keypoints;
+  // const kpSize = 5;
+  // const kpTextMargin = 2;
+  strokeWeight(1);
+  let xPoseLeftMost = width;
+  let xPoseRightMost = -1;
+  let yPoseTop = height;
+  let yPoseBottom = -1;
+  
+  const boundingBoxExpandFraction = 0.1;
+  let boundingBoxWidth = xPoseRightMost - xPoseLeftMost;
+  let boundingBoxHeight = yPoseBottom - yPoseTop;
+  let boundingBoxXMargin = boundingBoxWidth * boundingBoxExpandFraction;
+  let boundingBoxYMargin = boundingBoxHeight * boundingBoxExpandFraction;
+  xPoseRightMost += boundingBoxXMargin / 2;
+  xPoseLeftMost -= boundingBoxXMargin / 2;
+  yPoseTop -= boundingBoxYMargin / 2;
+  yPoseBottom += boundingBoxYMargin / 2;
+
+  noStroke();
+  fill(textColor);
+  textStyle(NORMAL);
+  textAlign(LEFT, BOTTOM);
+  const strPoseNum = "Pose: " + (poseIndex + 1);
+  text(strPoseNum, xPoseLeftMost, yPoseTop - textSize() - 1);
+  const strWidth = textWidth(strPoseNum);
+  textStyle(NORMAL);
+  text("Confidence: " + nf(pose.pose.score, 0, 1), xPoseLeftMost, yPoseTop);
 }
 
 function drawPose(pose, poseIndex) {
-  // Draw skeleton
-  const skeletonColor = color(255, 255, 255, 128);
-  stroke(skeletonColor); 
-  strokeWeight(2);
-  const skeleton = pose.skeleton;
-  for (let j = 0; j < skeleton.length; j += 1) {
-    const partA = skeleton[j][0];
-    const partB = skeleton[j][1];
-    line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
-  }
+  // // Draw skeleton
+  // const skeletonColor = color(255, 255, 255, 128);
+  // stroke(skeletonColor); 
+  // strokeWeight(2);
+  // const skeleton = pose.skeleton;
+  // for (let j = 0; j < skeleton.length; j += 1) {
+  //   const partA = skeleton[j][0];
+  //   const partB = skeleton[j][1];
+  //   line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
+  // }
+
+  //push();
+  fill("red"); // red nose
+  noStroke();
+
+  // Draw a circle for at the nose keypoint
+  circle(pose.pose.nose.x, pose.pose.nose.y, 40);
+  //pop();
+  fill("black");
+  noStroke();
+  circle(pose.pose.leftEye.x, pose.pose.rightEye.y, 50);
+  circle(pose.pose.rightEye.x, pose.pose.rightEye.y, 50);
   
   // Draw keypoints with text
   const kpFillColor = color(255, 255, 255, 200);
@@ -166,14 +195,14 @@ function drawPose(pose, poseIndex) {
       textAlign(RIGHT);
       xText = kp.position.x - (kpSize + kpTextMargin);
     }
-    textStyle(BOLD);
+    textStyle(NORMAL);
     text(kp.part, xText, yText);
     textStyle(NORMAL);
     yText += textSize();
-    text(int(kp.position.x) + ", " + int(kp.position.y), xText, yText);
+    // text(int(kp.position.x) + ", " + int(kp.position.y), xText, yText);
 
     yText += textSize();
-    text(nf(kp.score, 1, 2), xText, yText);
+    // text(nf(kp.score, 1, 2), xText, yText);
     //console.log(keypoint);
     // Only draw an ellipse is the pose probability is bigger than 0.2
     //if (keypoint.score > 0.2) {
@@ -182,36 +211,11 @@ function drawPose(pose, poseIndex) {
     stroke(kpOutlineColor);
     circle(kp.position.x, kp.position.y, kpSize);
   }
-
-  const boundingBoxExpandFraction = 0.1;
-  let boundingBoxWidth = xPoseRightMost - xPoseLeftMost;
-  let boundingBoxHeight = yPoseBottom - yPoseTop;
-  let boundingBoxXMargin = boundingBoxWidth * boundingBoxExpandFraction;
-  let boundingBoxYMargin = boundingBoxHeight * boundingBoxExpandFraction;
-  xPoseRightMost += boundingBoxXMargin / 2;
-  xPoseLeftMost -= boundingBoxXMargin / 2;
-  yPoseTop -= boundingBoxYMargin / 2;
-  yPoseBottom += boundingBoxYMargin / 2;
-  
-  noStroke();
-  fill(textColor);
-  textStyle(BOLD);
-  textAlign(LEFT, BOTTOM);
-  const strPoseNum = "Pose: " + (poseIndex + 1);
-  text(strPoseNum, xPoseLeftMost, yPoseTop - textSize() - 1);
-  const strWidth = textWidth(strPoseNum);
-  textStyle(NORMAL);
-  text("Confidence: " + nf(pose.pose.score, 0, 1), xPoseLeftMost, yPoseTop);
-
-  noFill();
-  stroke(kpFillColor);
-  rect(xPoseLeftMost, yPoseTop, boundingBoxWidth + boundingBoxXMargin, 
-    boundingBoxHeight + boundingBoxYMargin);
 }
 
 
 // A function to draw ellipses over the detected keypoints
-function drawKeypoints() {
+function drawKeypoints(poses) {
   // Loop through all the poses detected
   for (let i = 0; i < poses.length; i += 1) {
     // For each pose detected, loop through all the keypoints
